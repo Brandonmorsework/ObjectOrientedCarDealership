e91b8b1598b54167f65fa81da13ce40ca928b831
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-09 13:57:30 -0400

    Created methods inside Dealership class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,102 @@
 package com.pluralsight;
 
+
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> vehicle;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.vehicle = new ArrayList<>();
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double minPrice, double maxPrice) {
+
+
+
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByMake(String make) {
+
+
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int minYear, int maxYear) {
+
+
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+
+
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int minMileage, int maxMileage) {
+
+
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+
+
+
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+
+
+
+    }
+
+    public boolean addVehicle(Vehicle vehicle) {
+
+
+
+    }
+
+    public boolean removeVehicle(Vehicle vehicle) {
+
+
+
+    }
+
 }
