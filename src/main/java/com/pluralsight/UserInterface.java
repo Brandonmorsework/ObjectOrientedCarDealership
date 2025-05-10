421cbcd8ba083ac153bb07b27038018f1a0c1958
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-10 08:41:25 -0400

    Added methods structure to UserInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -4,4 +4,41 @@ public class UserInterface {
 
 
 
+
+    public void processGetByPriceRequest() {
+
+}
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processGetAllVehiclesRequest() {
+
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest() {
+
+    }
+
 }
