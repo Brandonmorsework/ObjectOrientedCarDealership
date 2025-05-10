41c9fef4e79c7d09499dca7f39f16dd9ddbc978e
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-10 13:22:47 -0400

    created switch case for display method inside the UserInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,13 +1,94 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
 
 
 
+    private Dealership dealership;
+
+    public void display() {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println(" ");
+            System.out.println("Welcome to the Car Dealership!");
+            System.out.println(" ");
+            System.out.println("Please Choose an option: ");
+            System.out.println(" ");
+            System.out.println("Press '1' to Find a Vehicle Within a Price Range");
+            System.out.println("Press '2' to Find a Vehicle by Make/Model");
+            System.out.println("Press '3' to Find a Vehicle Within a Year Range");
+            System.out.println("Press '4' to Find a Vehicle by Color");
+            System.out.println("Press '5' to Find a Vehicle by Mileage");
+            System.out.println("Press '6' to Find a Vehicle by Type (Car, Truck, SUV, Van)");
+            System.out.println("Press '7' to List All Vehicles");
+            System.out.println("Press '8' to Add a Vehicle to The Dealership");
+            System.out.println("Press '9' to Remove a Vehicle From The Dealership");
+            System.out.println("Press '0' to Quit");
+
+            String userInput = scanner.nextLine().trim();
+
+            switch (userInput.toUpperCase()) {
+                case "1":
+                    processGetByPriceRequest();
+                    break;
+                case "2":
+                    processGetByMakeModelRequest();
+                    break;
+                case "3":
+                    processGetByYearRequest();
+                    break;
+                case "4":
+                    processGetByColorRequest();
+                    break;
+                case "5":
+                    processGetByMileageRequest();
+                    break;
+                case "6":
+                    processGetByVehicleTypeRequest();
+                    break;
+                case "7":
+                    processGetAllVehiclesRequest();
+                    break;
+                case "8":
+                    processAddVehicleRequest();
+                    break;
+                case "9":
+                    processRemoveVehicleRequest();
+                    break;
+                case "0":
+                    System.out.println("Thank You For Using the Car Dealership, Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid Option, Returning Home...");
+                    break;
+
+            }
+
+        }
+
+        scanner.close();
+
+    }
+
+    private void init() {
+
+        String FILE_NAME = "Vehicles.csv";
+
+        DealershipFileManager dealershipFileManager = new DealershipFileManager();
+        this.dealership = dealershipFileManager.getDealership(FILE_NAME);
+    }
+
+
     public void processGetByPriceRequest() {
 
-}
+    }
 
     public void processGetByMakeModelRequest() {
 
